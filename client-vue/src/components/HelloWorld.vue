0e50693b7d866b5d183ee229df5ed2e741caf511
Jean-Baptiste Da Costa <32374126+ShyroeJB@users.noreply.github.com>
2020-01-29 12:20:54 +0100

    add vue client + update post to put in socket (#8)

---
+++ client-vue/src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+
+    <img v-if="info < 200" src="../img/gamellevide.jpg" alt="gamelle vide">
+    <img v-if="info > 200" src="../img/gamellepleine.jpg" alt="gamelle pleine">
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    // eslint-disable-next-line
+      this.axios
+      .get('https://localhost:8443/bowls/2')
+      .then(response => (this.info = response.data.waterLevel))
+      .catch(error => console.log(error))
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
